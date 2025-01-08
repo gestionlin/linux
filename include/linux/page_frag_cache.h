ec4cfe91670fd6f7a52f32b1be05c1abaa96a4b0
Yunsheng Lin <yunshenglin0825@gmail.com>
2025-01-08 22:34:05 +0800

    page_frag_cache

--- include/linux/page_frag_cache.h
+++ include/linux/page_frag_cache.h
@@ -27,6 +27,21 @@ static inline bool encoded_page_decode_pfmemalloc(unsigned long encoded_page)
 	return !!(encoded_page & PAGE_FRAG_CACHE_PFMEMALLOC_BIT);
 }
 
+static inline unsigned long encoded_page_decode_order(unsigned long encoded_page)
+{
+        return encoded_page & PAGE_FRAG_CACHE_ORDER_MASK;
+}
+
+static inline void *encoded_page_decode_virt(unsigned long encoded_page)
+{
+        return (void *)(encoded_page & PAGE_MASK);
+}
+
+static inline struct page *encoded_page_decode_page(unsigned long encoded_page)
+{
+        return virt_to_page((void *)encoded_page);
+}
+
 static inline void page_frag_cache_init(struct page_frag_cache *nc)
 {
 	nc->encoded_page = 0;
@@ -35,6 +50,40 @@ static inline void page_frag_cache_init(struct page_frag_cache *nc)
 static inline bool page_frag_cache_is_pfmemalloc(struct page_frag_cache *nc)
 {
 	return encoded_page_decode_pfmemalloc(nc->encoded_page);
+}
+
+static inline struct page *page_frag_cache_page(struct page_frag_cache *nc)
+{
+	return encoded_page_decode_page(nc->encoded_page);
+}
+
+static inline unsigned int page_frag_cache_remaining(struct page_frag_cache *nc)
+{
+	return (PAGE_SIZE << encoded_page_decode_order(nc->encoded_page)) -
+		nc->offset;
+}
+
+static inline unsigned int page_frag_cache_offset(struct page_frag_cache *nc)
+{
+	return nc->offset;
+}
+
+static inline void *page_frag_cache_virt(struct page_frag_cache *nc)
+{
+	return encoded_page_decode_virt(nc->encoded_page) + nc->offset;
+}
+
+static inline void page_frag_cache_commit_noref(struct page_frag_cache *nc,
+	      					unsigned int fragsz)
+{
+	nc->offset += fragsz;
+}
+
+static inline void page_frag_cache_commit(struct page_frag_cache *nc,
+					  unsigned int fragsz)
+{
+	page_frag_cache_commit_noref(nc, fragsz);
+	nc->pagecnt_bias--;
 }
 
 void page_frag_cache_drain(struct page_frag_cache *nc);
@@ -58,6 +107,20 @@ static inline void *page_frag_alloc(struct page_frag_cache *nc,
 				    unsigned int fragsz, gfp_t gfp_mask)
 {
 	return __page_frag_alloc_align(nc, fragsz, gfp_mask, ~0u);
+}
+
+static inline bool page_frag_cache_refill(struct page_frag_cache *nc,
+				     unsigned int fragsz, gfp_t gfp_mask)
+{
+	return __page_frag_cache_refill_align(nc, fragsz, gfp_mask, ~0u);
+}
+
+static inline bool page_frag_cache_refill_align(struct page_frag_cache *nc,
+					   unsigned int fragsz, gfp_t gfp_mask,
+					   unsigned int align)
+{
+	WARN_ON_ONCE(!is_power_of_2(align));
+	return __page_frag_cache_refill_align(nc, fragsz, gfp_mask, -align);
 }
 
 void page_frag_free(void *addr);
